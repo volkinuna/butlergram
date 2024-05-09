cf9dc5815f9a267264cce3749f02c0970dcfbc2d
bube29 <jaja29@naver.com>
2024-05-09 10:15:45 +0900

    2024-05-09 ES_1

--- src/main/java/com/butlergram/dto/UserUpdateDto.java
+++ src/main/java/com/butlergram/dto/UserUpdateDto.java
@@ -1,6 +1,7 @@
 package com.butlergram.dto;
 
 import com.butlergram.entity.Users;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
@@ -16,9 +17,13 @@ public class UserUpdateDto {
     @NotBlank(message = "이름은 필수 입력값입니다.")
     private String name; // 필수
 
+    private String username; //유저네임
+
     @NotEmpty(message = "비밀번호는 필수 입력값입니다.")
     @Length(min = 8, max = 16, message = "비밀번호는 8~16자 사이로 입력해 주세요.")
     private String password; // 필수
+
+    private String email; //이메일
 
     private String bio;
     private String phone;
